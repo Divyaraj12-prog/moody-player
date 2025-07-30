1a07152d5b57028133cc54c9ed03e64e04d697ad
Divyaraj Purohit <divyarajpurohit000@gmail.com>
2025-07-30 20:23:01 +0530

    updated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,12 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const SongRoutes = require('./routes/song.routes');
+const SongRoutes = require('../routes/song.routes');
 
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:3000',
+  'https://moody-player-tawny.vercel.app/'
+];
+
 app.use(cors({
-  origin: '*', 
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
 }));
 
