ab89973d699edce0d462520101544b701c480e7c
Divyaraj Purohit <divyarajpurohit000@gmail.com>
2025-07-30 20:06:30 +0530

    updated axios with render backend link

--- Frontend/src/axios.jsx
+++ Frontend/src/axios.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000/'
-    : 'https://moody-player-backend-uu8y.onrender.com'; // render wala url yha dena
+    : 'https://moody-player-xpvk.onrender.com'; // render wala url yha dena
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
