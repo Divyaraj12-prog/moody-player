6902375f190e80ef178029edd6d480c903b20c9d
Divyaraj Purohit <divyarajpurohit000@gmail.com>
2025-07-31 14:16:14 +0530

    Updated axios baseURL and fixed allowedOrigins in backend,

--- Frontend/src/axios.jsx
+++ Frontend/src/axios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const BASE_URL =
   process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000/'
+    ? 'http://localhost:3000'
     : 'https://moody-player-xpvk.onrender.com'; // render wala url yha dena
 
 const axiosInstance = axios.create({
