6fcc677fc9aa290e8639bef6760a82ef195ea8a2
Divyaraj Purohit <divyarajpurohit000@gmail.com>
2025-07-31 13:42:00 +0530

    Updated axios baseURL and fixed allowedOrigins in backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
-  'https://moody-player-tawny.vercel.app/'
+  'https://moody-player-tawny.vercel.app'
 ];
 
 app.use(cors({
