5d45295af2677be411d47a4755cce2abc8775ada
Divyaraj Purohit <divyarajpurohit000@gmail.com>
2025-08-04 13:47:11 +0530

    completedd

--- Frontend/src/components/FacialExpression.jsx
+++ Frontend/src/components/FacialExpression.jsx
@@ -5,6 +5,7 @@ import axiosInstance from '../axios.jsx';
 
 export default function FacialExpression({ setSongs }) {
   const videoRef = useRef();
+  const canvasRef = useRef();
 
   const loadModels = async () => {
     const MODEL_URL = './models';
@@ -21,9 +22,21 @@ export default function FacialExpression({ setSongs }) {
   };
 
   const detectMood = async () => {
+    const video = videoRef.current;
+    const canvas = canvasRef.current;
+
+    const displaySize = { width: video.videoWidth, height: video.videoHeight };
+    faceapi.matchDimensions(canvas, displaySize);
+
     const detections = await faceapi
-      .detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
       .withFaceExpressions();
+
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
 
     if (!detections || detections.length === 0) {
       console.log("No face detected");
@@ -52,17 +65,44 @@ export default function FacialExpression({ setSongs }) {
   };
 
   useEffect(() => {
-    loadModels().then(startVideo);
+    loadModels().then(() => {
+      startVideo();
+    });
   }, []);
 
   return (
-    <div className='VideoElement'>
+    <div className='VideoElement' style={{ position: 'relative' }}>
       <video
         ref={videoRef}
         autoPlay
         muted
         className='user-video-feed'
+        onPlay={async () => {
+          const interval = setInterval(async () => {
+            if (videoRef.current && canvasRef.current) {
+              const video = videoRef.current;
+              const canvas = canvasRef.current;
+
+              const displaySize = {
+                width: video.videoWidth,
+                height: video.videoHeight,
+              };
+
+              faceapi.matchDimensions(canvas, displaySize);
+
+              const detections = await faceapi
+                .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+                .withFaceExpressions();
+
+              const resizedDetections = faceapi.resizeResults(detections, displaySize);
+              canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+              faceapi.draw.drawDetections(canvas, resizedDetections);
+              faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+            }
+          }, 100); 
+        }}
       />
+      <canvas ref={canvasRef} className='overlay-canvas' />
       <button onClick={detectMood}>Detect Mood</button>
     </div>
   );
